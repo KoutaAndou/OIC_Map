db9d98a11e45eef324a99e93b20f9538023d6c47
OICNET\c0011 <c0011@oic.jp>
2023-06-20 17:13:20 +0900

    videoplayer追加

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -39,7 +39,7 @@ public class MenuController : MonoBehaviour
         if (isMenuVisible && playerTransform != null)
         {
             // メニューの位置と回転を固定した値に設定する
-            menuParent.transform.position = menuPosition + new Vector3(0,1.5f,0);
+            menuParent.transform.position = menuPosition + new Vector3(0,1.2f,0);
             menuParent.transform.rotation = menuRotation;
         }
     }
